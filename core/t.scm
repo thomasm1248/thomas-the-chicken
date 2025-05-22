3388ce9a01eead5f04a56febe8f78e63b02d53dd
thomasm1248 <thomasm1248@gmail.com>
2025-05-21 19:42:16 -0600

    changes

--- core/t.scm
+++ core/t.scm
@@ -1,12 +1,20 @@
 (import (chicken process-context)
         (chicken string)
-        (chicken load))
+        (chicken load)
+        (chicken file))
+
+(define (path->command-name path)
+    (substring path 0 (- (string-length path) 4)))
+
+(define (list-commands)
+    (map print
+        (map path->command-name (directory "tools"))))
 
 (let* ((args (command-line-arguments)))
     (if (< (length args) 1)
         (begin
-            (print "Usage: t <tool> [args...]")
-            (exit 1))
+            (print "Available commands:")
+            (list-commands))
         (let* ((filename (car args))
                (remaining-args (cdr args)))
             (load (conc "tools/" filename ".scm"))
@@ -15,5 +23,6 @@
                 ((eval 'main) remaining-args)
                 (begin
                     (print "Error: 'main' is not defined in the loaded file")
-                    (exit 1))))))
+                    (exit 1)))))
+)
 
--- tools/sys.scm
+++ tools/sys.scm
@@ -1,5 +1,10 @@
 (module tool
     (main)
     (import scheme (chicken base))
+
     (define (main args)
-        (print args)))
+        (if (= (length args) 0)
+            (print "No arguments were supplied to sys")
+            (case (car args)
+                (else (print "sys has no functionality yet.")))))
+)
